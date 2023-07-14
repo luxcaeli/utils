08b596a585fc0f77c18868b95a2e6be6d346092a
seungok Jeong <seungok.jeong@elicer.com>
2023-07-14 14:25:51 +0900

    :recycle: refactor(hexToRgba): refactor converting hex code to rgba color

--- src/hexToRgba.ts
+++ src/hexToRgba.ts
@@ -1,36 +1,41 @@
+const checkDecimalIsInvalid = (num: number) => {
+  return Number.isNaN(num) || num < 0 || num > 255;
+};
+
+const HEX_CODE_LENGTH = 7;
+
 /**
  * Covert hex color to rgba color
  *
- * @param color - The string value of hex color
+ * @param color
+ * The string value of hex color
+ * @param alpha - The number value of opacity
  * @example
- * `#FFF`, `#FFFFFF`
- * @param alpha - The number vlaue of opacity
- * @example
- * `0.5`
- * @returns Coverted rgba color
+ * ```typescript
+ * const rgbaColor = hexToRgba('#000', 0.5);
+ * ```
+ * @returns Converted rgba color
  */
-export const hexToRgba = (hexColor: string, alpha: number) => {
-  const isHexColor = hexColor[0] === "#" || ![4, 7].includes(hexColor.length);
+export const hexToRgba = (hexColor: string, alpha = 1) => {
+  const isHexColor = hexColor[0] === "#" && hexColor.length === HEX_CODE_LENGTH;
 
   if (!isHexColor) {
-    throw new Error(`It is not hex color. [${hexColor}]`);
+    throw new Error(`It is not hex color: [${hexColor}]`);
   }
 
   if (alpha < 0 || alpha > 1) {
-    throw new Error(`It is not valid opacity. [${alpha}]`);
+    throw new Error(`It is not valid opacity(0~1): [${alpha}]`);
   }
 
-  try {
-    const hex = hexColor.substring(1);
-    const rgb = hex.match(/.{1,2}/g)?.map((replacer) => parseInt(replacer, 16));
+  const hex = hexColor.substring(1);
+  const rgb = hex.match(/.{1,2}/g)?.map((replacer) => {
+    return parseInt(replacer, 16);
+  });
 
-    if (!rgb) {
-      throw new Error("There is nothing matched rgb color.");
-    }
-
-    const [r, g, b] = rgb;
-    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
-  } catch {
-    throw new Error(`It is not valid hexColor. [${hexColor}]`);
+  if (!rgb || rgb.findIndex(checkDecimalIsInvalid) > 0) {
+    throw new Error("There is nothing matched rgb color");
   }
+
+  const [r, g, b] = rgb;
+  return `rgba(${r}, ${g}, ${b}, ${alpha})`;
 };
